252c6230be684585fc01873d6e57abc635589b6d
Racheet Dave <racheet@gmail.com>
2014-05-05 14:04:03 -0400

    started working with data binds

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+var map_operations = void function() {
+    "use strict";
+    window.mapNodes = d3.selectAll("#map svg polygon, #map svg path");
 
+   $.getJSON("data/provinces.json", function (data) {
+       debugger;
+       window.provinceData = data;
+
+       window.mapNodes.data(window.provinceData, function (datum, index) {
+           if (this.getAttribute("id") === datum["name"]) {
+               return datum;
+           };
+       });
+   })
+    
+
+    
+} ()
