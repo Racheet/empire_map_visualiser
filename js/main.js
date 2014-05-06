5729bfa42ac59f1080ed87402d0038078a34b979
Racheet <racheet@gmail.com>
2014-05-06 16:59:21 +0000

    Added function for updating bottom box

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 var map_operations = void function() {
     "use strict";
     window.mapNodes = d3.selectAll("#map svg polygon, #map svg path");
-
+    
+    function updateBottomBox (title, data) {
+        if (typeof title === "string" && typeof data === "string") {
+            d3.select("#bottom_box").html("<h2>"+title+"</h2>").append("p").html(data);
+        } else {
+            throw new Error("Title or body is not a string!")
+        }
+        
+    };
+    
+    
    d3.json("data/provinces.json", function (err, data) {
        if(err) {throw err}
        else {
