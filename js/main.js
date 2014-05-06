11f9cd03383a9d25809754dd23e0e9b7d08e2d76
Racheet Dave <racheet@gmail.com>
2014-05-06 08:15:02 -0400

    switched from jquery to d3 for fetching json

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@ var map_operations = void function() {
     "use strict";
     window.mapNodes = d3.selectAll("#map svg polygon, #map svg path");
 
-   $.getJSON("data/provinces.json", function (data) {
-       window.provinceData = data;
-
-       window.mapNodes.data(window.provinceData, function (datum, index) {
-            return datum ? datum.name : this.getAttribute("id");
-       });
-       
+   d3.json("data/provinces.json", function (err, data) {
+       if(err) {throw err}
+       else {
+           window.provinceData = data;
+    
+           window.mapNodes.data(window.provinceData, function (datum, index) {
+                return datum ? datum.name : this.getAttribute("id");
+                
+           });
+       }
     });
     
 
