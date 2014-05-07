8dcaf7f6f74b7b3520ba56dffa1cb4568bb2bfb0
Racheet <racheet@gmail.com>
2014-05-07 18:22:59 +0000

    Wired up map/province data to right-nav boxes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var map_operations = void function() {
     "use strict";
     var mapNodes = d3.selectAll("#map svg polygon, #map svg path");
-    var bottomBox = d3.select("#bottom_box")
+    var bottomBox = d3.select("#bottom_box");
     
     function updateBottomBox (title, data) {
         if (typeof title === "string" && typeof data === "string") {
-            bottomBox.contents = d3.select("#bottom_box").selectAll("p, li, h2")
+            bottomBox.contents = d3.select("#bottom_box").selectAll("p, li, h2");
             bottomBox.contents.remove();
             
             bottomBox
@@ -15,20 +15,23 @@ var map_operations = void function() {
                  .append("p")
                  .html(data);
         } else {
-            throw new Error("Title or Data is not a string!")
-            
+            throw new Error("Title or Data is not a string!");
+        }
+    }
+      
     d3.json("data/provinces.json", function (err, data) {
-       if(err) {throw err}
+       if(err) { throw err; }
        else {
            window.provinceData = data;
     
-           window.mapNodes.data(window.provinceData, function (datum, index) {
-                return datum ? datum.name : this.getAttribute("id");
-                
+           mapNodes.data(window.provinceData, function (datum) {
+               return datum ? datum.name : this.getAttribute("id");
+           });
+           
+           mapNodes.on("click", function (data){
+               updateBottomBox(data.name,data.description);
            });
        }
     });
-    
-
-    
-} ()
+       
+} ();
