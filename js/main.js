e9b31d691c3ae359b09607f7b92d5bc6bb68e69c
Racheet <racheet@gmail.com>
2014-05-20 14:54:34 +0000

    refactored for readability

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var map_operations = void function() {
     "use strict";
-    var mapNodes = d3.selectAll("#map svg polygon, #map svg path");
-    var bottomBox = d3.select("#bottom_box");
-    var topBox = d3.select("#top_box")
+    
+    var mapNodes = d3.selectAll("#map svg polygon, #map svg path"),
+        bottomBox = d3.select("#bottom_box"),
+        topBox = d3.select("#top_box");
     
     function updateBox (box, data, title) {
         box.contents = box.selectAll("p, li, h2");
@@ -22,12 +23,20 @@ var map_operations = void function() {
       
     d3.json("data/provinces.json", function (err, data) {
         
-       if(err) return console.error(err);
+       if (err) return console.error(err);
         
        window.provinceData = data;
 
        mapNodes.data(window.provinceData, function (datum) {
-           return datum ? datum.name : this.getAttribute("id");
+           var key;
+           
+           if (datum.name) {
+               key = datum.name;
+           } else {
+               key = this.getAttribute("id");
+           }
+           
+           return key;
        });
 
        mapNodes.on("click", function (data){
