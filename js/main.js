b1918c5f5d2caed55a84ba9518053f753200b4b9
Racheet <racheet@gmail.com>
2014-05-24 13:03:54 +0000

    rolled back broken changes in main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 var map_operations = void function() {
     "use strict";
-    
-    var mapNodes = d3.selectAll("#map svg polygon, #map svg path"),
-        bottomBox = d3.select("#bottom_box"),
-        topBox = d3.select("#top_box");
+    var mapNodes = d3.selectAll("#map svg polygon, #map svg path");
+    var bottomBox = d3.select("#bottom_box");
+    var topBox = d3.select("#top_box")
     
     function updateBox (box, data, title) {
         box.contents = box.selectAll("p, li, h2");
@@ -23,20 +22,12 @@ var map_operations = void function() {
       
     d3.json("data/provinces.json", function (err, data) {
         
-       if (err) return console.error(err);
+       if(err) return console.error(err);
         
        window.provinceData = data;
 
        mapNodes.data(window.provinceData, function (datum) {
-           var key;
-           
-           if (datum.name) {
-               key = datum.name;
-           } else {
-               key = this.getAttribute("id");
-           }
-           
-           return key;
+           return datum ? datum.name : this.getAttribute("id");
        });
 
        mapNodes.on("click", function (data){
