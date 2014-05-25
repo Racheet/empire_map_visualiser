5616ce7752adfec014640d3b71ac013e355fd25a
Racheet <racheet@gmail.com>
2014-05-25 21:36:19 +0000

    added minimal display for nation of territory

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var map_operations = void function() {
+ var map_operations = void function() {
     "use strict";
     var mapNodes = d3.selectAll("#map svg polygon, #map svg path")
     , bottomBoxMiddle = d3.select("#bottom_box_middle")
@@ -7,13 +7,20 @@ var map_operations = void function() {
     , middleBoxRight = d3.select("#middle_box_right")
     , bottomBoxRight = d3.select("#bottom_box_right");
     
-    function updateBox (box, data, title) {
+    function updateBox (box, data, title, subhead) {
         box.contents = box.selectAll("p, li, h2, h3");
         box.contents.remove();
             
         if (typeof title === "string" && box === topBoxMiddle) {
-            box  .append("h2")
-                 .html(title);
+            box .append("h2")
+                .html(title);
+            
+            if (typeof subhead === "string") {
+                box  .append("p")
+                     .classed("subhead",true)
+                     .html(subhead)
+            }
+            
         }
         
         if (typeof title ==="string" && box !== topBoxMiddle) {
@@ -21,7 +28,7 @@ var map_operations = void function() {
                 .html(title);
         }
         
-        if (typeof data === "string") {
+        if (typeof data === "string" && data !== "") {
             box  .append("p")
                  .html(data);
         }
@@ -40,7 +47,7 @@ var map_operations = void function() {
 
        mapNodes.on("click", function (data){
            updateBox(bottomBoxMiddle,data.description,"Description");
-           updateBox(topBoxMiddle,"",data.name);
+           updateBox(topBoxMiddle,"",data.name,data.nation);
        });
     });
        
