e7f01d14f12382876e24d1907ae60a2c48cd559d
Racheet <racheet@gmail.com>
2014-05-26 00:17:12 +0000

    added function to toggle nation colours on and off

--- js/main.js
+++ js/main.js
@@ -34,21 +34,50 @@
         }
     }
     
-      
+    function toggleNationColours() { //could be slow, contains nested loops.
+        var nations = ["Navarr", "Highguard", "Dawn", "The Brass Coast", "The Marches", "Urizen", "The League", "Imperial Orcs", "Varushka", "Wintermark"];
+
+        nations.forEach(function (nation) {
+            var nationClass = nation.replace(/ /g,"_");
+            
+            mapNodes.attr("class", function (datum) {
+                if (datum.nation === nation){
+                    if(this.classList.contains(nationClass)){ 
+                        return this.classList.remove(nationClass).toString(); //we have the class already, remove it;
+                    } else if (this.classList.length > 0) {
+                        return this.classList.add(nationClass).toString();  //there are classes but not this one, so add it.
+                    } else {
+                        return nationClass; // there are no classes at all, so just return this class.
+                    }
+                }
+                
+                if (this.classList.length > 0) {
+                    return this.classList.toString(); //we have classes already, so return the same class string.
+                } 
+
+                return ""; // we had no classes, and we have no match. Set the classes to an empty string.
+
+            });
+
+        });
+    }
+
     d3.json("data/provinces.json", function (err, data) {
-        
-       if(err) return console.error(err);
-        
-       window.provinceData = data;
 
-       mapNodes.data(window.provinceData, function (datum) {
-           return datum ? datum.name : this.getAttribute("id");
-       });
+        if (err) return console.error(err);
 
-       mapNodes.on("click", function (data){
-           updateBox(bottomBoxMiddle,data.description,"Description");
-           updateBox(topBoxMiddle,"",data.name,data.nation);
-       });
+        window.provinceData = data;
+
+        mapNodes.data(window.provinceData, function (datum) {
+            return datum ? datum.name : this.getAttribute("id");
+        });
+
+        toggleNationColours();
+
+        mapNodes.on("click", function (data) {
+            updateBox(bottomBoxMiddle, data.description, "Description");
+            updateBox(topBoxMiddle, "", data.name, data.nation);
+        });
     });
-       
+      
 } ();
