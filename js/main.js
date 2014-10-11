25ff03934aed39f07c87bc0bf38d3a258595ae3d
Racheet <racheet@gmail.com>
2014-10-11 20:48:25 +0000

    added presentation of resources to provinces

--- js/main.js
+++ js/main.js
@@ -7,19 +7,49 @@
     , middleBoxRight = d3.select("#middle_box_right")
     , bottomBoxRight = d3.select("#bottom_box_right")
     , nations = ["Navarr", "Highguard", "Dawn", "The Brass Coast", "The Marches", "Urizen", "The League", "Imperial Orcs", "Varushka", "Wintermark"];
+     
+     
     
-    function updateBox (box, data, title, subhead) {
-        box.contents = box.selectAll("p, li, h2, h3");
+    
+    function updateBox (box, data, title, subhead, resources) {
+        var topDiv;
+        
+        function updateResources () {
+            var resourcebox = box.append("span").classed("resourceBox",true);
+            
+            if (resources.whiteGranite) {
+                resourcebox.append("span").classed("graniteNumber",true).text(resources.whiteGranite);
+                resourcebox.append("img").attr("src","../img/whitegranite.svg").classed("resource_img",true);
+            }
+            if (resources.weirwood) {
+                resourcebox.append("span").classed("weirwoodNumber",true).text(resources.weirwood);
+                resourcebox.append("img").attr("src","../img/weirwood.svg").classed("resource_img",true);
+            }
+            if (resources.mithril) {
+                resourcebox.append("span").classed("mithrilNumber",true).text(resources.mithril);
+                resourcebox.append("img").attr("src","../img/mithril.svg").classed("resource_img",true);
+                
+            }
+            if (resources.ilium) {
+                resourcebox.append("img").attr("src","../img/ilium.svg");
+            }
+        
+        }
+        
+        box.contents = box.selectAll("p, li, h2, h3, img, span");
         box.contents.remove();
             
         if (typeof title === "string" && box === topBoxMiddle) {
-            box .append("h2")
+            topDiv = box   .append("div")
+                           .classed("title_div",true);
+            
+            topDiv .append("h2")
                 .html(title);
             
             if (typeof subhead === "string") {
-                box  .append("p")
-                     .classed("subhead",true)
-                     .html(subhead);
+                topDiv .append("p")
+                       .classed("subhead",true)
+                       .html(subhead);
             }
             
         }
@@ -33,10 +63,33 @@
             box  .append("p")
                  .html(data);
         }
+        
+        if (resources) {
+           updateResources();
+        }
     }
      
      
     function addRow(rowName,data) {
+        
+        function regionResources(data) {
+            var output = [];
+            if (data.whiteGranite) {
+                output.push('<p class="resource"><img src="../img/whitegranite.svg" class="resource_img" /><strong>White Granite: </strong>' + data.whiteGranite + ' wains</p>');
+            }
+            if (data.weirwood) {
+                output.push('<p class="resource"><img src="../img/weirwood.svg" class="resource_img" /><strong>Weirwood: </strong>'+ data.weirwood + ' wains</p>');
+            }
+            if (data.mithril) {
+                output.push('<p class="resource"><img src="../img/mithril.svg" class="resource_img" /><strong>Mithril: </strong>'+ data.mithril + ' wains</p>');
+            }
+            if (data.illium) {
+                output.push('<p class="resource"><img src="../img/ilium.svg" class="resource_img" />illium found</p>');
+            }            
+            return output.join(' ');
+            
+        } 
+        
         //Find or Make a new Row
         var row =  d3.select("body").select('section#'+rowName);
         if(row.empty()) { 
@@ -54,6 +107,7 @@
         var boxes = boxBar.selectAll("div").data(data);
         var updateSelection = boxes.enter().append("div").attr("class","column large-3").append("div").attr("class","box feature_box");
         var heading = updateSelection.append("h3").text(function(d){return d.name;});
+        var resources = updateSelection.append("div").html(regionResources).classed("resources",true);
         var keywords = updateSelection.append("p").html(function (d){
             if (d.keywords) {
                 return "<strong>Keywords</strong>: "+d.keywords.join(',');
@@ -82,6 +136,17 @@
         
     }
      
+     function territoryResources(territoryData) {
+        return {
+            "ilium" : territoryData.ilium || false,
+            "weirwood" : territoryData.weirwood || 0,
+            "whiteGranite" : territoryData.whiteGranite || 0,
+            "mithril" : territoryData.mithril || 0,
+                 
+        };
+        
+    }
+     
     function toggleSelectedNode(node) {
           d3.selectAll(".selected").classed("selected",false);
           d3.select(node).classed("selected",true);
@@ -104,7 +169,7 @@
         mapNodes.on("click", function (data) {
             toggleSelectedNode(this);
             updateBox(bottomBoxMiddle, data.overview, "Description");
-            updateBox(topBoxMiddle, "", data.name, data.nation);
+            updateBox(topBoxMiddle, "", data.name, data.nation,territoryResources(data));
             updateBox(topBoxRight,data.history,"History");
             if (data.features.length) {
                 addRow("Features",data.features);
